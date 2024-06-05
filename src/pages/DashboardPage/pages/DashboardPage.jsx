d00df57df5d7b5edc2ee530044fd6b7d766c4a77
Juan Cruz <57047808+JuanCruzv98@users.noreply.github.com>
2024-06-05 09:52:38 -0600

    link to user

--- src/pages/DashboardPage/pages/DashboardPage.jsx
+++ src/pages/DashboardPage/pages/DashboardPage.jsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom"
+
 export const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
+   <>
+     <div>DashboardPage</div>
+     <Link to={"user"}>User</Link>
+   </>
+   
   )
 }
