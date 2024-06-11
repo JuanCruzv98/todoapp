ecf70b36cfc58baa9963c7d77af539314c914e63
Juan Cruz <57047808+JuanCruzv98@users.noreply.github.com>
2024-06-11 08:30:48 -0600

    actualizado

--- src/components/TodosArea.jsx
+++ src/components/TodosArea.jsx
@@ -1,37 +1,41 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react"
 import { useLocation } from "react-router-dom";
-import { AddTodo,TodoList } from "./";
-import { PaginationSimple } from "./PaginationSimple";
+import { AddTodo } from "./AddTodo"
+import { TodoList } from "./TodoList"
 import dummyAPI from "../api/todoAPI";
+import { PaginationSimple } from "./PaginationSimple";
+
 
 export const TodosArea = () => {
-  const location = useLocation();
+  const location = useLocation()
   const [todos,setTodos] = useState([]);
-  const [page,setPage] = useState(0);
+  const [page, setPage] = useState(1);
   
-  const startLoadingTodos = async ()=>{
+  const startToLoadTodos = async () => {
     const {data} = await dummyAPI.get(`/todos?limit=25&skip=${(page-1)*25}`);
-    if(data.todos){
+    
+    if(data?.todos){
+      console.log(data.todos)
       setTodos(data.todos);
     }
   }
-  useEffect(() => {
-    if(new URLSearchParams(location.search).get('page')!==null){
-      setPage(Number(new URLSearchParams(location.search).get('page')));
-    }
-  },[]);
 
-  useEffect(() => {
-    if(sessionStorage.getItem("username")){
-      startLoadingTodos();
+  useState(async () => {
+    if(new URLSearchParams(location.search).get('page')!==null){
+      setPage(Number(new URLSearchParams(location.search).get('page')))
     }
+  },[])
+  
+  useEffect(() => {
+    startToLoadTodos()
   }, [page]);
 
   return (
         <main className="todos-contaner">
             <AddTodo/>
             <TodoList todos={todos}/>
-            <PaginationSimple page={page} setPage={setPage} path={"/dashboard"}/>
+            <PaginationSimple page={page} setPage={setPage} path={'/dashboard'}/>
         </main>
   )
 }
+
--- src/components/TodosAreaUser.jsx
+++ src/components/TodosAreaUser.jsx
@@ -1,37 +1,40 @@
-import { useEffect,useState } from "react";
-import { useParams,useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
 import { TodoList } from './TodoList'
 import dummyAPI from '../api/todoAPI';
 import { PaginationSimple } from './PaginationSimple';
 
 export const TodosAreaUser = () => {
-  const {uid} = useParams();
   const location = useLocation();
+  const {uid} = useParams();
+  console.log(uid);
   const [todos,setTodos] = useState([]);
-  const [page,setPage] = useState(0);
+  const [page, setPage] = useState(1);
   
-  const startLoadingTodos = async ()=>{
+  const startToLoadTodos = async () => {
     const {data} = await dummyAPI.get(`/todos/user/${uid}?limit=25&skip=${(page-1)*25}`);
-    if(data.todos){
+    
+    if(data?.todos){
+      console.log(data.todos)
       setTodos(data.todos);
     }
   }
-  useEffect(() => {
+
+  useState(async () => {
     if(new URLSearchParams(location.search).get('page')!==null){
-      setPage(Number(new URLSearchParams(location.search).get('page')));
+      console.log(new URLSearchParams(location.search).get('page'))
+      setPage(Number(new URLSearchParams(location.search).get('page')))
     }
-  },[]);
-
+  },[])
+  console.log("hello");
   useEffect(() => {
-    if(sessionStorage.getItem("username")){
-      startLoadingTodos();
-    }
+    startToLoadTodos()
   }, [page]);
-
   return (
     <main className="todos-contaner mt-2">
         <TodoList todos={todos}/>
-        <PaginationSimple page={page} setPage={setPage} path={`/dashboard/user/${uid}`}/>
+        
+        <PaginationSimple page={page} setPage={setPage}  path={'/dashboard/user/'+uid}/>
     </main>
   )
 }
